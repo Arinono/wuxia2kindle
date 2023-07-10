aa306dd60716b65064f438e9f496857cc9173a78
Aurelien Arino <aurelien.arino@gmail.com>
2023-07-10 23:11:20 +0200

    feat(boost): init

---
+++ boost/boost.js
@@ -0,0 +1,95 @@
+const API = "http://localhost:3000/chapter"
+const METADATA_ATTR = "data-amplitude-params"
+
+const btn = document.createElement("div")
+btn.classList.add("wuxia2kindle-btn")
+btn.addEventListener('click', onClick)
+
+function safeParse(obj) {
+  try {
+    return JSON.parse(obj)
+  } catch (e) {
+    console.warn(e)
+    return null
+  }
+}
+
+function sleep(t) {
+  return new Promise(resolve => setTimeout(resolve, t * 1000))
+}
+
+function getContent() {
+  const contents = Array.from(document.querySelectorAll('.chapter-content p'))
+  const content = contents.map(el => el.innerText).join('\n\n')
+
+  return content
+}
+
+function green() {
+  base()
+  btn.classList.add("green")
+}
+
+function red(err) {
+  base()
+  btn.classList.add("red")
+  console.warn(err)
+}
+
+function base() {
+  btn.classList.remove("green")
+  btn.classList.remove("red")
+}
+
+function onClick() {
+  const dataContainer = document.querySelector(`[${METADATA_ATTR}]`)
+  const metadata = safeParse(dataContainer.getAttribute(METADATA_ATTR))
+  if (!safeParse) {
+    return
+  }
+
+  const content = getContent();
+
+  const chapter = {
+    book: metadata.novelName,
+    name: metadata.chapterTitle,
+    chapter_in_book: metadata.chapterNo,
+    content,
+  }
+
+  send(chapter)
+    .then(green)
+    .catch(red)
+    .finally(async () => {
+      await sleep(2)
+      base()
+    })
+}
+
+function send(chapter) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(API, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(chapter),
+      });
+
+      if (response.status >= 300) {
+        reject("failed to send")
+      } else {
+        resolve()
+      }
+    } catch (e) {
+      reject(e);
+    }
+  })
+}
+
+function loaded() {
+  document.body.appendChild(btn)
+}
+
+document.addEventListener("DOMContentLoaded", loaded)
