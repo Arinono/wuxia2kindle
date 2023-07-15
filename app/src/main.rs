149744499a170559b12eaa42f8f05a7f694d9b2b
Aurelien Arino <aurelien.arino@gmail.com>
2023-07-15 12:16:25 +0200

    make args optional and env_var override

--- app/src/main.rs
+++ app/src/main.rs
@@ -15,16 +15,16 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    #[command(arg_required_else_help = true)]
+    #[command(arg_required_else_help = false)]
     Ingest {
         #[arg(short, long)]
-        port: u16,
+        port: Option<u16>,
         #[arg(long)]
-        database_url: String,
+        database_url: Option<String>,
     },
     Worker {
         #[arg(long)]
-        database_url: String,
+        database_url: Option<String>,
     }
 }
 
@@ -33,10 +33,39 @@ fn main() {
 
     match args.command {
         Commands::Ingest { port, database_url } => {
-            ingest::start(port, database_url);
+            let env_port = std::env::var("PORT");
+            let env_db_url = std::env::var("DATABASE_URL");
+
+            let f_port = match env_port {
+                Ok(p) => p.parse::<u16>().unwrap(),
+                Err(_) => match port {
+                    Some(p) => p,
+                    None => 3000u16,
+                }
+            };
+
+            let db_url = match env_db_url {
+                Ok(u) => u,
+                Err(_) => match database_url {
+                    Some(u) => u,
+                    None => "postgres://localhost:5432/wuxia2kindle".to_owned(),
+                }
+            };
+
+            ingest::start(f_port, db_url);
         }
         Commands::Worker { database_url } => {
-            worker::start(database_url);
+            let env_db_url = std::env::var("DATABASE_URL");
+
+            let db_url = match env_db_url {
+                Ok(u) => u,
+                Err(_) => match database_url {
+                    Some(u) => u,
+                    None => "postgres://localhost:5432/wuxia2kindle".to_owned(),
+                }
+            };
+
+            worker::start(db_url);
         }
     }
 }
