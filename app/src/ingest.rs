378317e2ef953d477d2fd036082b6aebaa7d9bc8
Aurelien Arino <aurelien@arino.io>
2023-10-26 10:42:07 +0200

    fix adding book not adding chapter at the same time

--- app/src/ingest.rs
+++ app/src/ingest.rs
@@ -136,60 +136,66 @@ async fn add_chapter(
 ) -> impl IntoResponse {
     println!("Received chapter: {input}");
 
-    let book: Option<Book> = {
+    let mut o_book: Option<Book> = {
         sqlx::query_as!(Book, "SELECT * FROM books b WHERE b.name = $1", input.book,)
             .fetch_optional(&pool)
             .await
             .unwrap()
     };
 
-    match book {
-        None => {
-            println!("Inserting new book: {}", input.book);
-            sqlx::query!(
-                "INSERT INTO books (name, author, translator) VALUES ($1, $2, $3)",
-                input.book,
-                input.author,
-                input.translator,
-            )
-            .execute(&pool)
+    if o_book.is_none() {
+        println!("Inserting new book: {}", input.book);
+        sqlx::query!(
+            "INSERT INTO books (name, author, translator) VALUES ($1, $2, $3)",
+            input.book,
+            input.author,
+            input.translator,
+        )
+        .execute(&pool)
+        .await
+        .unwrap();
+    }
+
+    o_book = {
+        sqlx::query_as!(Book, "SELECT * FROM books b WHERE b.name = $1", input.book,)
+            .fetch_optional(&pool)
             .await
-            .unwrap();
-        }
-        Some(book) => {
-            println!("Inserting new chapter: {}", input);
-            if (sqlx::query!(
-                "INSERT INTO chapters (
-                    book_id,
-                    name,
-                    content,
-                    number_in_book
-                    ) VALUES ($1, $2, $3, $4)",
-                book.id,
-                input.name,
-                input.content,
-                input.number_in_book,
-            )
-            .execute(&pool)
-            .await)
-                .is_ok()
-            {
-                let count = match book.chapter_count {
-                    None => 1,
-                    Some(c) => c + 1,
-                };
-                sqlx::query!(
-                    "UPDATE books
-                    SET chapter_count = $2
-                    WHERE id = $1",
-                    book.id,
-                    count,
-                )
-                .execute(&pool)
-                .await
-                .unwrap();
-            }
-        }
+            .unwrap()
+    };
+
+    let book = o_book.expect("Book should exist");
+
+    println!("Inserting new chapter: {}", input);
+    if (sqlx::query!(
+        "INSERT INTO chapters (
+            book_id,
+            name,
+            content,
+            number_in_book
+            ) VALUES ($1, $2, $3, $4)",
+        book.id,
+        input.name,
+        input.content,
+        input.number_in_book,
+    )
+    .execute(&pool)
+    .await)
+        .is_ok()
+    {
+        let count = match book.chapter_count {
+            None => 1,
+            Some(c) => c + 1,
+        };
+        sqlx::query!(
+            "UPDATE books
+            SET chapter_count = $2
+            WHERE id = $1",
+            book.id,
+            count,
+        )
+        .execute(&pool)
+        .await
+        .unwrap();
     }
 
     (
