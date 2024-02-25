0ceb4143456fa0827ca85d353d29e55f8b2138f0
Aurelien Arino <aurelien@arino.io>
2024-02-25 12:21:51 +0100

    sentry ?

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,14 @@ struct Args {}
 
 fn main() {
     let _args = Args::parse();
-
     let env = Environment::new();
+
+    if let Ok(sentry_dsn) = std::env::var("SENTRY_DSN") {
+        let _guard = sentry::init((sentry_dsn, sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        }));
+    }
 
     server::start(env);
 }
